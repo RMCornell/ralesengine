09ce0ac8e941b26ae51ce22e71653e999d441f71
RMCornell <zen.alchemist5280@gmail.com>
2015-07-27 21:36:44 -0600

    added items api v1 controller and tested index and show through postman.  Removed create update and destroy actions as they do not appear to be part of the base functionality of application requirements

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -8,22 +8,4 @@ class Api::V1::CustomersController < ApplicationController
   def show
     respond_with Customer.find_by(id: params[:id])
   end
-
-  def create
-    respond_with Customer.create(customer_params)
-  end
-
-  def update
-    respond_with Customer.update(params[:id], customer_params)
-  end
-
-  def destroy
-    respond_with Customer.destroy(params[:id])
-  end
-
-  private
-
-  def customer_params
-    params.require(:customer).permit(:first_name, :last_namea)
-  end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,22 +8,5 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     respond_with Merchant.find_by(id: params[:id])
   end
-
-  def create
-    respond_with Merchant.create(merchant_params)
-  end
-
-  def update
-    respond_with Merchant.update(params[:id], merchant_params)
-  end
-
-  def destroy
-    respond_with Merchant.destroy(params[:id])
-  end
-
-  private
-
-  def merchant_params
-    params.require(:merchant).permit(:name)
-  end
 end
+
