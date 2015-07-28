4ce39eacecdeebeb77770dfd4461b75787d41edd
RMCornell <zen.alchemist5280@gmail.com>
2015-07-27 21:01:37 -0600

    api v1 customer controller added and tested through post man for index and show functions

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::CustomersController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Customer.all
+  end
+
+  def show
+    respond_with Customer.find_by(id: params[:id])
+  end
+
+  def create
+    respond_with Customer.create(customer_params)
+  end
+
+  def update
+    respond_with Customer.update(params[:id], customer_params)
+  end
+
+  def destroy
+    respond_with Customer.destroy(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_namea)
+  end
+end
