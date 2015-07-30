25e6d633ce0a9ebfe75516102a0af0ab520fce4f
RMCornell <zen.alchemist5280@gmail.com>
2015-07-29 23:25:17 -0600

    invoice controllers spec completed

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoicesController, type: :controller do
+  context '#index' do
+    it 'returns all invoices' do
+      customer = Customer.create(first_name: "First", last_name: "Last")
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
 
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+
+      invoices = JSON.parse(response.body)
+      expect(invoices.count).to eq(1)
+
+      invoice_response = invoices.first
+      # expect(invoice_response['customer_id']).to eq(1)
+      # expect(invoice_response['merchant_id']).to eq(1)
+      expect(invoice_response['status']).to eq('success')
+    end
+  end
+
+  context '#show' do
+    it 'shows an invoice' do
+      customer = Customer.create(first_name: "First", last_name: "Last")
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
+
+      get :show, id: invoice.id, :format => :json
+
+      expect(response).to have_http_status(:ok)
+
+      invoice_response = JSON.parse(response.body)
+      # expect(invoice_response['customer_id']).to eq(1)
+      # expect(invoice_response['merchant_id']).to eq(1)
+      expect(invoice_response['status']).to eq('success')
+    end
+  end
 end
