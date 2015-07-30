5cafad6e5bb29b46a170b0b93251d9936e6d632a
RMCornell <zen.alchemist5280@gmail.com>
2015-07-30 10:15:49 -0600

    added database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,19 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-RSpec.configure do |config|
+require 'database_cleaner'
 
+
+RSpec.configure do |config|
   config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
@@ -26,9 +36,6 @@ RSpec.configure do |config|
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
