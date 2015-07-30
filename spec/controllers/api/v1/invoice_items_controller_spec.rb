b56562f8f799a25ad6728907b1de568b7043f12b
RMCornell <zen.alchemist5280@gmail.com>
2015-07-30 00:10:31 -0600

    remaining controller tests completed

--- spec/controllers/api/v1/invoice_items_controller_spec.rb
+++ spec/controllers/api/v1/invoice_items_controller_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoiceItemsController, type: :controller do
+  context '#index' do
+    it 'returns all invoice items' do
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
 
+      customer = Customer.new(first_name: "First", last_name: "Last")
+      customer.save
+
+      item = Item.new(name: "Item", description: 'Description', unit_price: 100, merchant_id: merchant.id)
+      item.save
+
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
+
+      invoice_item = InvoiceItem.new(item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: 100)
+      invoice_item.save
+
+      get :index, format: :json
+
+      expect(response). to have_http_status(:ok)
+
+      invoice_items = JSON.parse(response.body)
+      expect(invoice_items.count).to eq(1)
+
+      invoice_item = invoice_items.first
+      # expect(invoice_item['item_id']).to eq(1)
+      # expect(invoice_item['invoice_id']).to eq(1)
+      expect(invoice_item['quantity']).to eq(1)
+      expect(invoice_item['unit_price']).to eq(100)
+    end
+  end
+
+  context '#show' do
+    it 'shows an invoice item' do
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
+
+      customer = Customer.new(first_name: "First", last_name: "Last")
+      customer.save
+
+      item = Item.new(name: "Item", description: 'Description', unit_price: 100, merchant_id: merchant.id)
+      item.save
+
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
+
+      invoice_item = InvoiceItem.new(item_id: item.id, invoice_id: invoice.id, quantity: 1, unit_price: 100)
+      invoice_item.save
+
+      get :show, id: invoice_item.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+
+      invoice_item_response = JSON.parse(response.body)
+      # expect(invoice_item['item_id']).to eq(1)
+      # expect(invoice_item['invoice_id']).to eq(1)
+      expect(invoice_item['quantity']).to eq(1)
+      expect(invoice_item['unit_price']).to eq(100)
+    end
+  end
 end
--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TransactionsController, type: :controller do
+  context '#index' do
+    it 'returns all transactions' do
+      customer = Customer.create(first_name: "First", last_name: "Last")
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
 
+      transaction = Transaction.new(invoice_id: invoice.id, result: 'success', credit_card_number: 4242424242424242)
+      transaction.save
+
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+
+      transactions = JSON.parse(response.body)
+      expect(transactions.count).to eq(1)
+
+      transaction = transactions.first
+      # expect(transaction['invoice_id']).to eq(1)
+      expect(transaction['result']).to eq('success')
+      expect(transaction['credit_card_number']).to eq(4242424242424242)
+    end
+  end
+
+  context '#show' do
+    it 'shows a transaction' do
+      customer = Customer.create(first_name: "First", last_name: "Last")
+      merchant = Merchant.new(name: 'Merchant')
+      merchant.save
+      invoice = Invoice.new(customer_id: customer.id, merchant_id: merchant.id, status: 'success')
+      invoice.save
+
+      transaction = Transaction.new(invoice_id: invoice.id, result: 'success', credit_card_number: 4242424242424242)
+      transaction.save
+
+      get :show, id: transaction.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+
+      transaction_response = JSON.parse(response.body)
+      # expect(transaction_response['invoice_id']).to eq(1)
+      expect(transaction_response['result']).to eq('success')
+      expect(transaction_response['credit_card_number']).to eq(4242424242424242)
+    end
+  end
 end
