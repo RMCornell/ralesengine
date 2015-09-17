e2df8b23ec8a5a4f19ade86da6e0e6117c206f57
RMCornell <zen.alchemist5280@gmail.com>
2015-09-16 21:10:12 -0600

    4 tests failing.  GTFE

--- db/migrate/20150916184913_fix_unit_price.rb
+++ db/migrate/20150916184913_fix_unit_price.rb
@@ -1,7 +1,7 @@
 class FixUnitPrice < ActiveRecord::Migration
   def change
-    change_column :invoice_items, :unit_price, :decimal, precision: 8, scale: 2
-    change_column :items, :unit_price, :decimal, precision: 8, scale: 2
+    change_column :invoice_items, :unit_price, :decimal
+    change_column :items, :unit_price, :decimal
 
   end
 end
